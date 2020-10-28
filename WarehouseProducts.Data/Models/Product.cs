0a03e8fe9797293cd172e656bf1811fc68b46b47
AlexsandrRybalko <rybalkosanya20@gmail.com>
2020-10-28 17:41:28 +0200

    Add project files.

---
+++ WarehouseProducts.Data/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseProducts.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ WarehouseProducts.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseProducts.Domain.Models;
+
+namespace WarehouseProducts.Domain
+{
+    public class ProductRepository
+    {
+        private List<Product> Products { get; set; }
+
+        public ProductRepository()
+        {
+            Products = new List<Product>();
+        }
+
+        public void Create(Product model)
+        {
+            Products.Add(model);
+        }
+
+        public Product GetById(int id)
+        {
+            foreach(var p in Products)
+            {
+                if(p.Id == id)
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
+    }
+}
